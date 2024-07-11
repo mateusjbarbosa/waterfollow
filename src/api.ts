8b9380aacb6ba47fc6647bafd2043e2dbaaee5fb
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-07-11 15:28:31 -0300

    feat: insert new registry on hydration history table at API POST route

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,15 @@
 import Fastify from 'fastify';
+import { db } from './db';
 import { ENV } from './env';
+import { hydrationHistory } from './schema';
 
 const fastify = Fastify({
   logger: true
+})
+
+fastify.post('/', async (_, reply) => {
+  await db.insert(hydrationHistory).values({ hydrationAt: new Date(Date.now()), quantityInMilliliters: 300 });
+  reply.code(201).send({ success: true, message: "Hydration updated successfully!" })
 })
 
 fastify.listen({ port: ENV.PORT }, function (err) {
