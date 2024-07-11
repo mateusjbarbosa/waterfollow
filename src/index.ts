bf258fb2280963b885a19d0d60d0209034091946
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-07-11 11:32:58 -0300

    feat: prepare initial version of the reminder
    Reminder is sent every hour at 30 minute

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Waterfollow")
+import cron from 'node-cron';
+
+function sendReminderHourly() {
+  cron.schedule('30 * * * *', () => {
+    console.log(`${Intl.DateTimeFormat('pt-BR', { dateStyle: 'short', timeStyle: 'short' }).format(Date.now())} - Reminder sent`);
+  }, { timezone: "America/Sao_Paulo" });
+}
+
+sendReminderHourly();
