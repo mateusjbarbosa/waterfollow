93ef031fe1f3c36dacd865984f94a76b5b5858a6
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-07-12 09:09:18 -0300

    chore: update env

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,23 @@
 import z from "zod";
 
-const envSchema = z.object({
-  DB_HOST: z.string().trim().min(1),
-  DB_NAME: z.string().trim().min(1),
-  DB_PASSWORD: z.string().trim().min(1),
-  DB_PORT: z.coerce.number(),
-  DB_USER: z.string().trim().min(1),
-  NODE_ENV: z.enum(["development", "production", "test"]),
+const defaultEnvSchema = z.object({
+  DB_URL: z.string().trim().url(),
+  NODE_ENV: z.enum(["development", "staging", "production"]),
   PORT: z.coerce.number(),
   RESEND_API_KEY: z.string().trim().min(1),
 });
+
+const stagingEnvSchema = z.object({
+  SUPABASE_API_KEY: z.string().trim().min(1)
+})
+
+let envSchema;
+
+if (["staging", "production"].includes(process.env.NODE_ENV!)) {
+  envSchema = defaultEnvSchema.merge(stagingEnvSchema)
+} else {
+  envSchema = defaultEnvSchema
+}
 
 type Env = z.infer<typeof envSchema>;
 export const ENV: Env = envSchema.parse(process.env);
