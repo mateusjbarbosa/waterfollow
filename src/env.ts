03426b60d76b60738b0de670f309063746075485
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-07-12 10:23:29 -0300

    feat: revert env

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,11 @@
 import z from "zod";
 
-const defaultEnvSchema = z.object({
+const envSchema = z.object({
   DB_URL: z.string().trim().url(),
   NODE_ENV: z.enum(["development", "staging", "production"]),
   PORT: z.coerce.number(),
   RESEND_API_KEY: z.string().trim().min(1),
-});
-
-const stagingEnvSchema = z.object({
-  SUPABASE_API_KEY: z.string().trim().min(1)
 })
-
-let envSchema;
-
-if (["staging", "production"].includes(process.env.NODE_ENV!)) {
-  envSchema = defaultEnvSchema.merge(stagingEnvSchema)
-} else {
-  envSchema = defaultEnvSchema
-}
 
 type Env = z.infer<typeof envSchema>;
 export const ENV: Env = envSchema.parse(process.env);
