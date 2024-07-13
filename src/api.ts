d6dbba2dfcf94f1a0e0519338cd1ecbab67bb106
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-07-13 11:21:53 -0300

    feat: filter hydration history by today on get route

--- src/api.ts
+++ src/api.ts
@@ -1,57 +1,92 @@
-import Fastify from 'fastify';
-import { ZodError, z } from 'zod';
-import { version } from '../package.json';
-import { db } from './db';
-import { ENV } from './env';
-import { HydrationHistory, hydrationHistory } from './schema';
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { and, gte, lt } from "drizzle-orm";
+import Fastify from "fastify";
+import { ZodError, z } from "zod";
+import { version } from "../package.json";
+import { db } from "./db";
+import { ENV } from "./env";
+import { HydrationHistory, hydrationHistory } from "./schema";
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 const newHydrationRegistrySchema = z.object({
-  quantityInMilliliters: z.number()
-})
+  quantityInMilliliters: z.number(),
+});
 
-fastify.get('/', (_, reply) => {
-  reply.send({success: true, message: `Waterfollow API running | v${version}`})
-})
+fastify.get("/", (_, reply) => {
+  reply.send({
+    success: true,
+    message: `Waterfollow API running | v${version}`,
+  });
+});
 
-fastify.post('/hydrations', async (request, reply) => {
+fastify.post("/hydrations", async (request, reply) => {
   try {
-    const data = newHydrationRegistrySchema.parse(request.body)
+    const data = newHydrationRegistrySchema.parse(request.body);
 
     const newRegistry: HydrationHistory = {
-      quantityInMilliliters: data.quantityInMilliliters
-    }
-    const result = await db.insert(hydrationHistory).values(newRegistry).returning();
-    reply.code(201).send({success: true, data: result})
-  } catch(err) {
+      quantityInMilliliters: data.quantityInMilliliters,
+    };
+    const result = await db
+      .insert(hydrationHistory)
+      .values(newRegistry)
+      .returning();
+    reply.code(201).send({ success: true, data: result });
+  } catch (err) {
     if (err instanceof ZodError) {
-      reply.status(400).send({success: false, errors: err.issues.map(issue => ({ path: issue.path.join(), message: issue.message}))})
+      reply
+        .status(400)
+        .send({
+          success: false,
+          errors: err.issues.map((issue) => ({
+            path: issue.path.join(),
+            message: issue.message,
+          })),
+        });
     }
-    console.log(err)
-    reply.code(500).send({success: false, message: "Internal server error"})
+    console.log(err);
+    reply.code(500).send({ success: false, message: "Internal server error" });
   }
-})
+});
 
-fastify.get('/hydrations', async (_, reply) => {
+fastify.get("/hydrations", async (_, reply) => {
   try {
-    const result: HydrationHistory[] = await db.select().from(hydrationHistory);
-    reply.send({success: true, data: result})
-  } catch(err) {
-    console.log(err)
-    reply.code(500).send({success: false, message: "Internal server error"})
+    dayjs.extend(utc);
+    const todayAtMidnight = dayjs()
+      .utc()
+      .set("hour", 0)
+      .set("minute", 0)
+      .set("second", 0);
+    const tomorrowAtMidnight = todayAtMidnight.add(1, "day");
+    const result: HydrationHistory[] = await db
+      .select()
+      .from(hydrationHistory)
+      .where(
+        and(
+          gte(hydrationHistory.hydrationAt, todayAtMidnight.toDate()),
+          lt(hydrationHistory.hydrationAt, tomorrowAtMidnight.toDate())
+        )
+      );
+    reply.send({ success: true, data: result });
+  } catch (err) {
+    console.log(err);
+    reply.code(500).send({ success: false, message: "Internal server error" });
   }
-})
+});
 
 function isStagingOrProductionEnv() {
-  return ["staging", "production"].includes(ENV.NODE_ENV)
+  return ["staging", "production"].includes(ENV.NODE_ENV);
 }
 
-fastify.listen({ port: ENV.PORT, host: isStagingOrProductionEnv() ? '0.0.0.0' : undefined }, function (err) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+fastify.listen(
+  { port: ENV.PORT, host: isStagingOrProductionEnv() ? "0.0.0.0" : "" },
+  function (err) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
   }
-})
+);
